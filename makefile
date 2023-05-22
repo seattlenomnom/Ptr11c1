7d940f0a6eb10bbfea2f67c036780197c99ae942
seattlenomnom <mark.crapser@gmail.com>
2023-05-21 21:33:20 -0700

    first commit

---
+++ makefile
@@ -0,0 +1,5 @@
+# makefile for Ptr11c1
+#
+Ptr11c1: ptr11c1.c
+	gcc -g ptr11c1.c -o ./Build/ptr11c1
+
---
+++ ptr11c1.c
@@ -0,0 +1,10 @@
+/* ptr11c1 - program to illustrate pointers */
+
+#include <stdio.h>
+
+int main(void){
+    int count = 10, x;
+    int *int_pointer;
+
+
+}
